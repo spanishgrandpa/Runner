293faccbcbfc1d8080238352c37410b9de7ca3e6
spanishgrandpa <81233832+spanishgrandpa@users.noreply.github.com>
2022-05-18 21:29:50 +0700

    жил-был ёжик, который дышал попой, шёл по лесу, сел на пенёк и задохнулся

--- app/src/main/java/com/charkosoff/runner/StartScreen.kt
+++ app/src/main/java/com/charkosoff/runner/StartScreen.kt
@@ -12,11 +12,16 @@ import android.os.VibrationAttributes
 import android.util.AttributeSet
 import android.view.View
 import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class StartScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_screen)
+
+        val backgrounds = listOf(R.drawable.splactwp0, R.drawable.splactwp1, R.drawable.splactwp2, R.drawable.splactwp3, R.drawable.splactwp4, R.drawable.splactwp5, R.drawable.splactwp6, R.drawable.splactwp7, R.drawable.splactwp8, R.drawable.splactwp9)
+        var view = findViewById<ConstraintLayout>(R.id.layout)
+        view.setBackgroundResource(backgrounds.random())
 
         findViewById<Button>(R.id.button2).setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
